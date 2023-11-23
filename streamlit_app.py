6e901496b83bc9ed2653e69cd3781617d0f49b09
Mehranwaheed <119947085+Mehranwaheed@users.noreply.github.com>
2023-11-23 14:22:29 +0000

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+fig = px.scatter_mapbox(df_copy2, lat="Latitude", lon="Longitude_x",color="Life expectancy", size="Population",
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=20,zoom=12)
+#fig.show()
+st.plotly_chart(fig, use_container_width=True)
